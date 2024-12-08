cee55ab8108c75941d462ad90b8d387bfa8736ed
Paolo Arroyo <juanpaoloarroyo@gmail.com>
2024-12-08 13:15:50 +0800

    feat: services page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,7 +1,43 @@
-const Services = () => {
+'use client';
+
+import { default as Services } from "@/components/Services";
+import { BsGlobe2, BsShop, } from "react-icons/bs";
+import { MdOutlineDesignServices } from "react-icons/md";
+import { PiRankingFill } from "react-icons/pi";
+
+const services = [
+  {
+    icon: <BsGlobe2 />,
+    title: 'Web Development',
+    description: 'I build websites and web applications that are fast, secure, and responsive.',
+    href: '/services/web-development',
+  },
+  {
+    icon: <BsShop />,
+    title: 'E-Commerce Development',
+    description: 'I specialize in building e-commerce websites using Shopify and other platforms.',
+    href: '/services/web-development',
+  },
+  {
+    icon: <MdOutlineDesignServices />,
+    title: 'UI/UX Design',
+    description: 'I design user interfaces and experiences that are intuitive and visually appealing.',
+    href: '/services/web-design',
+  },
+  {
+    icon: <PiRankingFill />,
+    title: 'SEO Optimization',
+    description: 'I optimize websites for search engines to improve their visibility and ranking.',
+    href: '/services/seo-optimization',
+  }
+];
+
+const ServicesPage = () => {
   return (
-    <div>Services</div>
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <Services services={services} />
+    </section>
   )
 }
 
-export default Services;
+export default ServicesPage;
---
+++ src/components/Services.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowDownRight, BsGlobe2 } from 'react-icons/bs';
+
+type ServicesProps = {
+  services: {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+    href: string;
+  }[];
+};
+
+const Services = ({ services }: ServicesProps) => {
+  return (
+    <div className="container mx-auto">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ 
+          opacity: 1,
+          transition: { delay: 0.2, duration: 0.4, ease: "easeIn" },
+        }}
+        className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+      >
+        {services.map((service, index) => {
+          return (
+            <div key={index} className="flex flex-1 flex-col justify-start gap-6 group">
+              <div className="flex flex-col gap-8 group/link">
+                <div className='w-full flex justify-between items-center'>
+                  <div className="text-4xl font-extrabold text-outline"> {service.icon} </div>
+                  <Link href={service.href} className="w-[28px] h-[28px] rounded-full bg-white group-hover:bg-accent transition-all duration-300 flex justify-center items-center group-hover/link:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-xl" />
+                  </Link>
+                </div>
+                <Link href={service.href} className="group/link group-hover:text-accent">
+                  <h2> {service.title} </h2>
+                </Link>
+              </div>
+              <p className="h-full text-white/80"> {service.description} </p>
+              <div className="border-b border-white/20 w-full" />
+            </div>
+          )
+        })}
+      </motion.div>
+    </div>
+  )
+}
+
+export default Services;
