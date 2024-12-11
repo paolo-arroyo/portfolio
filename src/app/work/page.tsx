b0efe282448789d6233e91bceaec6562694080e8
Paolo Arroyo <juanpaoloarroyo@gmail.com>
2024-12-11 09:52:47 +0800

    feat: project tags

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -6,8 +6,11 @@ import React, { useState } from 'react';
 import "swiper/css";
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 
+import { FaTrophy } from 'react-icons/fa';
+
 import { BsArrowRight, BsArrowUpRight, BsGithub } from 'react-icons/bs';
 
+import ProjectCategory from '@/components/ProjectCategory';
 import ProjectStack from '@/components/ProjectStack';
 import SliderButtons from '@/components/SliderButtons';
 import { Button } from '@/components/ui/button';
@@ -19,39 +22,39 @@ const projects = [
   {
     title: 'Novita Knits',
     description: 'Novita Knits is the largest knitting supplier in Western Europe. Alongside e-commerce consultants and their in-house development team, I helped build a new e-commerce solution for their online store using a cutting-edge approach with Shopify Hydrogen and Sanity CMS. Novita won the Grand One Finland Award for Best in User Experience, Best Technical Implementation, and three other categories.',
-    category: ['E-Commerce', 'Fullstack', 'Team'],
+    category: ['ecommerce', 'fullstack', 'awardee'],
     image: '/assets/project-novita.jpg',
     href: 'https://novita.com',
     stack: ['hydrogen', 'remix', 'react', 'typescript', 'tailwind', 'sanity', 'html', 'css'],
   },
   {
     title: 'Origin USA',
-    description: 'Origin USA is a premier manufacturer of American-made fitness equipment. I worked with their in-house development team develop new features for their Shopify store, including a redesign of the produt and collection pages. I also helped optimize their site for performance and SEO.',
-    category: ['E-Commerce', 'Fullstack'],
+    description: 'Origin USA is a premier manufacturer of American-made fitness equipment. I worked with their in-house development team develop new features for their Shopify store. I also helped optimize their site for performance and SEO.',
+    category: ['ecommerce', 'fullstack'],
     image: '/assets/project-novita.png',
     href: 'https://originusa.com',
     stack: ['react', 'shopify', 'javascript', 'tailwind'],
   },
   {
     title: 'Lintbells Veterinary',
-    description: 'Origin USA is a premier manufacturer of American-made fitness equipment. I worked with their in-house development team develop new features for their Shopify store, including a redesign of the produt and collection pages. I also helped optimize their site for performance and SEO.',
-    category: ['E-Commerce', 'Fullstack'],
+    description: 'Lintbells Veterinary is a leading pet supplement brand in the UK. I worked with their in-house development team to build a new Shopify store with analytics and tracking features.',
+    category: ['ecommerce', 'fullstack'],
     image: '/assets/project-novita.png',
     href: 'https://originusa.com',
     stack: ['shopify', 'typescript', 'tailwind', 'googleanalytics', 'html', 'css'],
   },
   {
     title: 'Honest Paws',
-    description: 'Origin USA is a premier manufacturer of American-made fitness equipment. I worked with their in-house development team develop new features for their Shopify store, including a redesign of the produt and collection pages. I also helped optimize their site for performance and SEO.',
-    category: ['E-Commerce', 'Frontend'],
+    description: 'As Senior Engineer at One Pet, I helped build and optimize the Honest Paws e-commerce store on Shopify, including bespoke product page and subscription features that improved conversion rates by approximately 40%.',
+    category: ['ecommerce', 'frontend'],
     image: '/assets/project-novita.png',
     href: 'https://originusa.com',
     stack: ['react', 'shopify', 'javascript', 'tailwind'],
   },
   {
     title: 'Marimekko',
-    description: 'Origin USA is a premier manufacturer of American-made fitness equipment. I worked with their in-house development team develop new features for their Shopify store, including a redesign of the produt and collection pages. I also helped optimize their site for performance and SEO.',
-    category: ['E-Commerce', 'Fullstack'],
+    description: 'I was part of the multinational team that helped Marimekko, a Finnish design house and international lifestyle brand, integrate their e-commerce systems following MACH architecture principles.',
+    category: ['mach', 'ecommerce', 'fullstack'],
     image: '',
     href: 'https://originusa.com',
     stack: ['next', 'contentful', 'typescript', 'tailwind', 'sanity', 'html', 'css'],
@@ -79,13 +82,7 @@ const Work = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-1/2 xl:h-[460px] flex flex-col xl:justify-start gap-6 order-2 xl:order-none">
-            <div className="flex flex-row gap-4">
-              {activeProject.category.map((category, index) => {
-                return (
-                  <span key={index} className="text-accent text-sm font-semibold bg-accent/10 px-2 py-1 rounded-full"> {category}</span>
-                )
-              })}
-            </div>
+            <ProjectCategory categories={activeProject.category} />
             <Link href={activeProject.href}>
               <h2 className="text-4xl font-bold hover:text-accent transition-all duration-300">
                 {activeProject.title} 
---
+++ src/components/ProjectCategory.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { FaShoppingCart, FaTrophy } from 'react-icons/fa';
+import { FiLayout } from 'react-icons/fi';
+import { MdArchitecture } from 'react-icons/md';
+import { RiStackFill } from 'react-icons/ri';
+
+const projectCategory = {
+  ecommerce: {
+    name: 'E-Commerce',
+    icon: <FaShoppingCart />
+  },
+  fullstack: {
+    name: 'Fullstack',
+    icon: <RiStackFill />
+  },
+  awardee: {
+    name: 'Award-Winning',
+    icon: <FaTrophy />
+  },
+  frontend: {
+    name: 'Frontend',
+    icon: <RiStackFill />
+  },
+  mach: {
+    name: 'MACH Architecture',
+    icon: <MdArchitecture />
+  },
+};
+
+type ProjectCategoryProps = {
+  categories?: ["ecommerce" | "fullstack" | "awardee" | "frontend" | "mach"];
+}
+
+const ProjectCategory = ({ categories }: ProjectCategoryProps) => {
+  if (!categories) return null;
+  return (
+    <div className="flex flex-row gap-4">
+      {categories.map((category, index) => {
+        return (
+          <span key={index} className="text-accent text-sm font-semibold bg-accent/10 px-2 py-1 rounded-full flex flex-row items-center gap-2">
+            {projectCategory[category].icon ? projectCategory[category].icon : null}
+            {projectCategory[category].name}
+          </span>
+        )
+      })}
+    </div>
+  )
+}
+
+export default ProjectCategory;
